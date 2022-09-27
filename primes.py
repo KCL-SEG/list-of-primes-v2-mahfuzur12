804bc7cf55aad5f3c9ac3d1823a58c3235ecd2f9
daddyyogmafs <K21038343@kcl.ac.uk>
2022-09-27 21:07:48 +0100

    Revert "Exercise complete"
    This reverts commit 27a8b7d0e4d8321eed9b4bcf40f3726756dbeba6.

--- primes.py
+++ primes.py
@@ -2,19 +2,5 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = [2]
-    counter = 0
-    number = 3
-    while counter < number_of_primes - 1:
-        for i in range (2, number):
-            if (number % i != 0):
-                list.append(number)
-                counter += 1
-                number += 1
-            else:
-                number += 1
-
-    print(list)
+    list = []
     return list
-
-primes(10)
