27a8b7d0e4d8321eed9b4bcf40f3726756dbeba6
daddyyogmafs <K21038343@kcl.ac.uk>
2022-09-27 21:05:25 +0100

    Exercise complete

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    list = [2]
+    counter = 0
+    number = 3
+    while counter < number_of_primes - 1:
+        for i in range (2, number):
+            if (number % i != 0):
+                list.append(number)
+                counter += 1
+                number += 1
+            else:
+                number += 1
+
+    print(list)
     return list
+
+primes(10)
